8d777f8e918dd5a0659605f8cd397698c2d74c7e
dzr <942217633@qq.com>
2018-05-30 22:38:46 +0800

    core 2.1 demo

---
+++ GI/GI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
